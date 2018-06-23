e1cb505bd838a9eb2bd2eba6f7be04a052ab5a44
Timothy Smith <timsmith78@yahoo.com>
2018-06-23 16:19:53 -0500

    Added basic search form and query for USDA food ID (ndbno)

---
+++ food-log.js
@@ -0,0 +1,30 @@
+const apiKey = "PPZaLIjNkv3RxMDe5YUDrBG7Tt7VdpUOlXHCGxnk"
+const searchUrlBase = "https://api.nal.usda.gov/ndb/search/?format=json&"
+
+$("#submit").click( (clickEvt) => {
+    clickEvt.preventDefault();
+    let food = $("#food-entry").val()
+    
+   $ .ajax({
+       url: searchUrlBase + "&q=" + food + "&api_key=" + apiKey,
+       method: "GET"
+   }).then( (foodListResponse) => {
+       console.log(foodListResponse)
+       let foodList = foodListResponse.list.item;
+       console.log(foodList)
+       for (var i = foodListResponse.list.start; i <= foodListResponse.list.end; ++i) {
+            let newRow = $("<tr>")
+            let newFood = $("<td>")
+                .attr("scope","row")
+                .attr("class","food-elem")
+                .attr("ndbno",foodList[i].ndbno)
+                .text(foodList[i].name)
+            let newManu = $("<td>")
+                .text(foodList[i].manu)
+            newRow.append(newFood)
+            newRow.append(newManu)
+            $("tbody").append(newRow)
+       }
+   })
+
+})
