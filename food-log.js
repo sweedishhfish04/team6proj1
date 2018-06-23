d6257941d6a40a1834e7a0e30797e4f53484cc58
Timothy Smith <timsmith78@yahoo.com>
2018-06-23 17:03:34 -0500

    Added hover highlighting to search results

--- food-log.js
+++ food-log.js
@@ -1,30 +1,36 @@
 const apiKey = "PPZaLIjNkv3RxMDe5YUDrBG7Tt7VdpUOlXHCGxnk"
 const searchUrlBase = "https://api.nal.usda.gov/ndb/search/?format=json&"
 
-$("#submit").click( (clickEvt) => {
+$("#submit").click((clickEvt) => {
     clickEvt.preventDefault();
     let food = $("#food-entry").val()
-    
-   $ .ajax({
-       url: searchUrlBase + "&q=" + food + "&api_key=" + apiKey,
-       method: "GET"
-   }).then( (foodListResponse) => {
-       console.log(foodListResponse)
-       let foodList = foodListResponse.list.item;
-       console.log(foodList)
-       for (var i = foodListResponse.list.start; i <= foodListResponse.list.end; ++i) {
+
+    $.ajax({
+        url: searchUrlBase + "&q=" + food + "&api_key=" + apiKey,
+        method: "GET"
+    }).then((foodListResponse) => {
+        let foodList = foodListResponse.list.item;
+        for (var i = foodListResponse.list.start; i < foodListResponse.list.end; ++i) {
             let newRow = $("<tr>")
             let newFood = $("<td>")
-                .attr("scope","row")
-                .attr("class","food-elem")
-                .attr("ndbno",foodList[i].ndbno)
+                .attr("scope", "row")
+                .attr("class", "food-elem")
+                .attr("ndbno", foodList[i].ndbno)
                 .text(foodList[i].name)
             let newManu = $("<td>")
                 .text(foodList[i].manu)
             newRow.append(newFood)
             newRow.append(newManu)
             $("tbody").append(newRow)
-       }
-   })
+        }
+        $(".food-elem").hover(
+            (hoverEvt) => {
+                $(hoverEvt.target).attr("class", "food-elem text-blue")
+            },
+            (hoverEvt) => {
+                $(hoverEvt.target).attr("class", "food-elem")
+            }
+        )
+    })
 
 })
