50f81ce11afdb6d05877b690b3c7a0a3f0593173
Timothy Smith <timsmith78@yahoo.com>
2018-06-23 20:18:51 -0500

    Added API to fetch and display nutritional data per quantity entered

--- food-log.js
+++ food-log.js
@@ -1,5 +1,17 @@
+// API access constants
 const apiKey = "PPZaLIjNkv3RxMDe5YUDrBG7Tt7VdpUOlXHCGxnk"
 const searchUrlBase = "https://api.nal.usda.gov/ndb/search/?format=json&"
+const foodReportUrlBase = "https://api.nal.usda.gov/ndb/V2/reports?ndbno="
+
+// Nutrient ids
+const caloriesID = 208
+const proteinID = 203
+const fatID = 204
+const carbID = 205
+const sugarID = 269
+
+// Nutrient array
+const loggedNutrients = [caloriesID, proteinID, fatID, carbID, sugarID]
 
 $("#submit").click((clickEvt) => {
     clickEvt.preventDefault();
@@ -9,6 +21,7 @@ $("#submit").click((clickEvt) => {
         url: searchUrlBase + "&q=" + food + "&api_key=" + apiKey,
         method: "GET"
     }).then((foodListResponse) => {
+        $("#search-tbl").empty()
         let foodList = foodListResponse.list.item;
         for (var i = foodListResponse.list.start; i < foodListResponse.list.end; ++i) {
             let newRow = $("<tr>")
@@ -21,16 +34,55 @@ $("#submit").click((clickEvt) => {
                 .text(foodList[i].manu)
             newRow.append(newFood)
             newRow.append(newManu)
-            $("tbody").append(newRow)
+            $("#search-tbl").append(newRow)
         }
-        $(".food-elem").hover(
-            (hoverEvt) => {
-                $(hoverEvt.target).attr("class", "food-elem text-blue")
-            },
-            (hoverEvt) => {
-                $(hoverEvt.target).attr("class", "food-elem")
-            }
-        )
+
+        $(".food-elem").click( (clickEvt) => {
+            let ndbnoKey = $(clickEvt.target).attr("ndbno")
+
+            $.ajax({
+                url: foodReportUrlBase + ndbnoKey + "&api_key=" + apiKey,
+                method: "GET"
+            }).then( (foodDbResponse) => {
+                console.log(foodDbResponse)
+                let nutrients = foodDbResponse.foods[0].food.nutrients
+                console.log(nutrients)
+                quantityDiv = $("#quantity")
+                quantityDiv.append($("<h3>").text("Enter Quantity:"))
+                quantityDiv.append($("<p>").text(foodDbResponse.foods[0].food.desc.name))
+                quantityDiv.append($("<input>") 
+                    .attr("id", "qty")    
+                    .attr("type", "text")
+                    .attr("size", "5")
+                    .attr("class", "d-inline"))
+                quantityDiv.append($("<p>").text(nutrients[0].measures[0].label)
+                    .attr("class", "d-inline"))
+                quantityDiv.append($("<button>").text("OK")
+                    .attr("class", "d-inline")
+                    .attr("id", "okBtn"))
+                window.scrollTo(0,0)
+                $("#okBtn").click( (okEvt) => {
+                    okEvt.preventDefault()
+                    let newRow = $("<tr>")
+                    let name = $("<td>").text(foodDbResponse.foods[0].food.desc.name)
+                    newRow.append(name)
+                    let quantity = $("#qty").val()
+                    let quantityStr = quantity + " " + nutrients[0].measures[0].label
+                    let quantityDiv = $("<td>").text(quantityStr)
+                    newRow.append(quantityDiv)
+                    for (let i = 0; i < loggedNutrients.length; ++i) {
+                        for (let j = 0; j < nutrients.length; ++j) {
+                            if (nutrients[j].nutrient_id == loggedNutrients[i]) {
+                                nutrientVal = quantity * nutrients[j].measures[0].value
+                                newRow.append($("<td>").text(nutrientVal + " " + nutrients[j].unit))
+                            }
+                        }
+                    }
+                    $("#log-tbl").append(newRow)
+                    $("#quantity").empty()
+                })
+            })
+        })
     })
 
 })
